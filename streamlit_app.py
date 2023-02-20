09e91aa3821fe558843b6addff0b64782fe37697
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-20 08:46:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,43 +8,22 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Función para generar un artículo a partir de citas
 def generate_article(quotes):
-    # Formatear las citas como una lista con viñetas
-    formatted_quotes = ["• " + re.sub(r'[\n\r]+', ' ', quote) for quote in quotes]
-    # Unir las citas en un solo texto y generar las paráfrasis
-    prompt = "\n".join([f"{paraphrase} ({source})" for source, paraphrase in paraphrase_quotes(formatted_quotes)])
-    # Generar el artículo usando GPT-3
+    prompt = f"Here are some quotes:\n\n{quotes}\n\nPlease write an article based on these quotes:\n"
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="text-davinci-002",
         prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
         temperature=0.7,
-        max_tokens=324,
-        top_p=1,
-        frequency_penalty=0.3,
-        presence_penalty=0.3
     )
     # Devolver el texto generado
     return response.choices[0].text.strip()
 
-def paraphrase_quotes(quotes):
-    # Usar OpenAI GPT-3 para generar paráfrasis de las citas
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Please paraphrase the following quotes in APA format:\n\n{quotes}\n",
-        max_tokens=324,
-        temperature=0.7,
-        n=1,
-        stop=None
-    )
-    # Procesar las respuestas del modelo
-    output = response.choices[0].text.strip().split("\n")
-    paraphrases = [re.search(r"(?<=• )(.*)", o).group(0) for o in output]
-    sources = [re.search(r"(?<=\(from ).*(?=\))", o).group(0) for o in output]
-    return list(zip(sources, paraphrases))
-
 # Interfaz de usuario
 st.title("Generador de artículos con GPT-3")
 
-st.write("Ingrese citas para generar un artículo original (máximo: 5 citas):")
+st.write("Ingrese citas para generar un artículo original:")
 
 # Textarea para ingresar las citas
 quotes = st.text_area("Citas")
