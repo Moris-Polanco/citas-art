76be2460857319dbedbacfef0c6358ee69e7db95
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-20 08:36:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,38 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Función para generar un artículo a partir de citas
 def generate_article(quotes):
-    prompt = f"Here are some quotes:\n\n{quotes}\n\nPlease write an article that includes at least some of these quotes:\n"
+    # Formatear las citas como una lista con viñetas
+    formatted_quotes = ["• " + re.sub(r'[\n\r]+', ' ', quote) for quote in quotes]
+    # Unir las citas en un solo texto y generar las paráfrasis
+    prompt = "\n".join([f"{paraphrase} ({source})" for source, paraphrase in paraphrase_quotes(formatted_quotes)])
+    # Generar el artículo usando GPT-3
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=3048,
-        n=1,
-        stop=None,
-        temperature=0.7,
+        temperature=0.5,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
     # Devolver el texto generado
     return response.choices[0].text.strip()
+
+def paraphrase_quotes(quotes):
+    # Usar OpenAI GPT-3 para generar paráfrasis de las citas
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Please paraphrase the following quotes in APA format:\n\n{quotes}\n",
+        max_tokens=1024,
+        temperature=0.5,
+        n=1,
+        stop=None
+    )
+    # Procesar las respuestas del modelo
+    output = response.choices[0].text.strip().split("\n")
+    paraphrases = [re.search(r"(?<=• )(.*)", o).group(0) for o in output]
+    sources = [re.search(r"(?<=\(from ).*(?=\))", o).group(0) for o in output]
+    return list(zip(sources, paraphrases))
 
 # Interfaz de usuario
 st.title("Generador de artículos con GPT-3")
